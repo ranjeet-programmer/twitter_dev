da6b24bcbc5cb8514117356bbcfbf4642bf4474d
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2023-03-20 19:16:24 +0530

    added hashtag model

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -5,33 +5,18 @@ const tweetSchema = new mongoose.Schema(
     content: {
       type: String,
       required: true,
+      max: [250, "Tweet cannot be more than 250 characters"],
     },
 
-    userEmail: {
-      type: String,
-    },
-
-    comments: [
+    hasttags: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Comment",
+        ref: "Hashtag",
       },
     ],
   },
   { timestamps: true }
 );
-
-tweetSchema.virtual("contentWithEmail").get(function process() {
-  return `${this.content} \n  created by  ${this.userEmail}`;
-});
-
-tweetSchema.pre("save", function (next) {
-  console.log("Inside a hook");
-
-  this.content = this.content + "....";
-
-  next();
-});
 
 const Tweet = mongoose.model("Tweet", tweetSchema);
 
