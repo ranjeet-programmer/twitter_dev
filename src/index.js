86fa56b6ed361548163840b963fbaaea947a0689
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2023-03-23 09:08:50 +0530

    Added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -6,19 +6,10 @@ const app = express();
 
 const PORT = 3000;
 
-const TweetRepository = require("./repository/tweet-repository");
-const Comment = require("./models/comment");
-
 app.listen(PORT, async () => {
   console.log(` App started on port ${PORT}`);
 
   await connect();
 
   console.log("mongodb connected");
-
-  const tweetRepo = new TweetRepository();
-
-  const tweet = await tweetRepo.create({ content: "New with hooks" });
-
-  console.log(tweet);
 });
---
+++ src/services/tweet-service.js
@@ -0,0 +1,42 @@
+const { TweetRepository, HashtagRepository } = require("../repository/index");
+
+class TweetService {
+  constructor() {
+    this.tweetRepository = new TweetRepository();
+    this.hashtagRepository = new HashtagRepository();
+  }
+
+  async create(data) {
+    const content = data.content;
+
+    let tags = content.match(/#[a-zA-Z0-9]+/g);
+
+    tags = tags.map((tag) => tag.substring(1));
+
+    const tweet = await this.tweetRepository.create(data);
+
+    let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+
+    let titleOfPresentTags = alreadyPresentTags.map((tags) => tags.title);
+
+    let newTags = tags.filter((tag) => !titleOfPresentTags.includes(tag));
+
+    newTags = newTags.map((tag) => {
+      return {
+        title: tag,
+        tweets: [tweet.id],
+      };
+    });
+
+    await this.hashtagRepository.bulkCreate(newTags);
+
+    alreadyPresentTags.forEach((tag) => {
+      tag.tweets.push(tweet.id);
+      tag.save();
+    });
+
+    return tweet;
+  }
+}
+
+module.exports = TweetService;
