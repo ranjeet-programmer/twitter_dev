9b28f20758b96ef724dee926ca9e3885837ffd33
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2023-03-25 10:04:27 +0530

    files added successfully

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 import { connect } from "./config/database.js";
 
-import services from "./services/tweet-service.js";
+import apiRoutes from "./routes/index.js";
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api", apiRoutes);
 
 const PORT = 3000;
 
@@ -11,10 +17,6 @@ app.listen(PORT, async () => {
   console.log(` App started on port ${PORT}`);
 
   await connect();
-
-  let ser = new services();
-
-  await ser.create({ content: "capital #IF " });
 
   console.log("mongodb connected");
 });
