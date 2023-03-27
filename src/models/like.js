01ac8fbcf259dba3ad9653743547bd4d277945dc
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2023-03-27 10:52:28 +0530

    added the user and like model

---
+++ src/models/like.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    onModel: {
+      type: String,
+      required: true,
+      enum: ["Tweet", "Comment"],
+    },
+
+    likeable: {
+      type: mongoose.Schema.types.ObjectId,
+      required: true,
+      refPath: "onModel",
+    },
+
+    user: {
+      type: mongoose.Schema.types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Like = mongoose.model("Like", likeSchema);
+
+export default Like;
